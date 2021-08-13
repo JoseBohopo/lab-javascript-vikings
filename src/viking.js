fb5b18225016fb26a820c3d8009b63645a29f307
jose ramon <rabohopo@gmail.com>
2021-08-13 18:34:55 +0200

    done

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,80 @@
 // Soldier
-class Soldier {}
+class Soldier{
+  
+  constructor(health, strength) {
+    this.health = health
+    this.strength = strength
+  }
+  attack(){
+    return this.strength
+  }
+  receiveDamage(dmg) {
+    this.health = this.health - dmg
+  }
+ }
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+
+  constructor(name,health,strength){
+    super(health,strength)
+    this.name = name
+  }
+
+  receiveDamage(dmg) {
+    this.health = this.health - dmg
+    if(this.health > 0){
+      return `${this.name} has received ${dmg} points of damage`;
+    }else{
+      return `${this.name} has died in act of combat`
+    }
+ }
+
+ battleCry(){
+   return "Odin Owns You All!"
+}
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+ 
+  receiveDamage(dmg){
+    this.health = this.health - dmg
+
+    if ( health > 0) {
+      return `A Saxon has received ${dmg} points of damage`;
+    } else {
+      return "A Saxon has died in combat"
+    }
+
+  }
+  saxonDies = function(){
+    return "Odin Owns You All!"
+ }
+}
 
 // War
-class War {}
+class War {
+  vikingArmy = []
+  saxonArmy = []
+  addViking = function (viking) {
+    this.vikingArmy.push(viking)
+  }
+  addSaxon = function (saxon) {
+    this.saxonArmy.push(saxon)
+   
+  }
+  vikingAttack = function () {
+    expect(this.viking).toEqual(this.saxon);
+  }
+  saxonAttack = function () {
+    
+  }
+  showStatus = function () {
+    
+  }
+}
 
 
 
